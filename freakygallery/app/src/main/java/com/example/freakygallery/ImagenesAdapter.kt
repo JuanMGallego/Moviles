edd9a9bd9450c88b4f97f481c6f1e2bd39e2ce90
JuanMGallego <jmgallego@iesnervion.es>
2023-10-17 14:40:40 +0200

    Imagenes puestas en FrikeyGallery

--- freakygallery/app/src/main/java/com/example/freakygallery/ImagenesAdapter.kt
+++ freakygallery/app/src/main/java/com/example/freakygallery/ImagenesAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.freakygallery
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.freakygallery.databinding.ItemImagenBinding
 
 class ImagenesAdapter(
@@ -11,10 +14,24 @@ class ImagenesAdapter(
     class ViewHolder(private val binding: ItemImagenBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(imagen: Imagen) {
+        fun setImage(imagen: Imagen) {
 
-            
-
+            Glide
+                .with(binding.root.context)
+                .load(imagen.src)
+                .into(binding.imagenVista)
         }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemImagenBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = imagenes.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.setImage(imagenes[position])
     }
 }
--- freakygallery/app/src/main/java/com/example/freakygallery/MainActivity.kt
+++ freakygallery/app/src/main/java/com/example/freakygallery/MainActivity.kt
@@ -12,19 +12,23 @@ class MainActivity : AppCompatActivity() {
         val imagenes = ImagenesBinding.inflate(layoutInflater)
         setContentView(imagenes.root)
 
-        imagenes.vistaImagenes.adapter = ImagenesAdapter(listOf(
-            Imagen("https://loremflickr.com/320/240/cat?lock=1")
-        ), object : ContactoPulsadoListener{
-
-            override fun contactoPulsado(contacto: Contacto) {
-                val dial = Intent(
-                    Intent.ACTION_DIAL,
-                    Uri.parse("tel:" + contacto.tfno)
-                )
-                startActivity(dial)
-            }
-
-        })
-
+        imagenes.vistaImagenes.adapter = ImagenesAdapter(
+            listOf(
+                Imagen("https://loremflickr.com/320/240/cat?lock=1"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=2"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=3"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=4"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=5"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=6"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=7"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=8"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=9"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=10"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=12"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=13"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=14"),
+                Imagen("https://loremflickr.com/320/240/cat?lock=15")
+            )
+        )
     }
 }
